2418cf6ed09ab1d5cd95492ecccd4bcb306818e8
Maria Espino <127848953+mgsetler@users.noreply.github.com>
2023-09-05 11:48:38 -0600

    Add files via upload

---
+++ CE1, Functions/index.js
@@ -0,0 +1,27 @@
+function combine(a, b, c) {
+	return (a * b) / c;
+}
+
+// --- !! SUCCESS !!--- ///
+
+// --- INSTRUCTIONS --- ///
+// Exercise: Working with Functions
+// Your task is to write a new function that should be named combine and have the following characteristics:
+
+// - Accept three input values
+
+// - Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+// - Return the calculated result
+
+// --- HINTS  --- //
+
+// - Create a new function via the function keyword
+
+// - Make sure the name contains no typo (it should be combine)
+
+// - Separate multiple parameters via commas
+
+// - Use the * and / operators for the required calculation
+
+// - Don't forget to return the calculated result via the return keyword
---
+++ CE3, Working with JSX Code/apps.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+// don't change the Component name "App"
+export default function App() {
+	return (
+		<div>
+			<h1>Exercise done!</h1>
+		</div>
+	);
+}
+
+/*
+Exercise: Working with JSX Code
+This is a quick exercise to get started with React and JSX code: Your task is to edit the JSX code of a provided React component such that it displays a h1 element with the text "Exercise done!" on the page.
+
+Important: The custom JSX code must be added inside the existing <div> element.
+
+HINTS:
+Make sure to add your <h1> element inside of the existing <div> element
+
+Watch out for typos - the solution is checked automatically (and hence typos cause it to fail)
+
+*/
