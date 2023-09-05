148e0736906fed74012941dda866625aeee3994c
Maria Espino <127848953+mgsetler@users.noreply.github.com>
2023-09-05 11:50:48 -0600

    Add files via upload

---
+++ javascript-refresher-start-forked/scripts/app.js
@@ -0,0 +1,50 @@
+// NOTE: keeping the old code on file just in case
+// import { apiKey } from "./utils.js";
+
+//console.log(utils.apiKey);
+
+/// -----
+
+// FOR DEFAULT IMPORT CONFIG
+// import apiKey from "./utils.js";
+
+//console.log(utils.apiKey);
+
+/// -----
+
+// FOR importing clusters
+// import { apiKey, abc } from "./utils.js";
+
+//console.log(utils.apiKey);
+
+/// -----
+
+//better version & cleaner version to import clusters
+//import * as utils from "./utils.js";
+
+//console.log(utils.apiKey);
+
+//for assigning Aliases
+//import { apiKey, abc as content } from "./utils.js";
+
+//console.log(content);
+
+/// -----
+
+//console.log("Hello, World!");
+
+// ----- operators
+
+//console.log(10 + 5);
+//console.log(10 - 5);
+//console.log(10 * 5);
+//console.log(10 / 5);
+
+// -----functions
+
+function greet(userName, message) {
+  console.log(userName);
+  console.log(message);
+}
+
+greet();
---
+++ javascript-refresher-start-forked/scripts/utils.js
@@ -0,0 +1,6 @@
+// NOTE: keeping the old code on file just in case
+// export let apiKey = "asdfghjkl";
+
+export default "asdfghjkl";
+export let apiKey = "asdfghjkl";
+export let abc = "abc";
